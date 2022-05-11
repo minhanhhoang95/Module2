3670353c019f09a14d14a166abc14e278f14ba65
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-05-11 17:24:00 +0700

    update task1-10

---
+++ Java/src/Week1/task10.java
@@ -0,0 +1,15 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class task10 {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mời bạn nhập usd :");
+        usd = sc.nextDouble();
+        double quydoi=usd*23000;
+        System.out.println("giá trị VND : " + quydoi);
+    }
+}
