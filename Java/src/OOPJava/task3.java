423664dfe82b122be60059ec7e0ed77ee2340f82
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-05-18 14:22:49 +0700

    "fix"

--- Java/src/OOPJava/task3.java
+++ Java/src/OOPJava/task3.java
@@ -1,23 +1,42 @@
 package OOPJava;
 
+import java.util.Arrays;
 import java.util.Random;
 
 
 public class task3 {
-    public class StopWatch {
+    public static void main(String[] args) {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        System.out.printf("Start: %d\n", watch.getStartTime());
+        watch.stop();
+        System.out.printf("End: %d\n", watch.getEndTime());
+        System.out.printf("Elapsed Time: %d\n", watch.getElapsedTime());
+//        System.out.println(Arrays.toString(watch.randomArray()));
+
+    }
+
+    public static class StopWatch {
         private long startTime;
         private long endTime;
 
-        public long getElapsedTime() {
-            return endTime - startTime;
+        public long getStartTime(){
+            return startTime;
         }
-
+        public long getEndTime(){
+            return endTime;
+        }
+        public long getElapsedTime() {
+            long elapsed;
+            elapsed = endTime - startTime ;
+            return elapsed ;
+        }
         public int[] randomArray() {
             int[] array = new int[100000];
             for (int i = 0; i < 100000; i++) {
                 array[i] = (new Random()).nextInt(100000) + 1;
             }
-            return array;
+            return array ;
         }
 
         public void selectionSort(int[] arr) {
@@ -35,11 +54,11 @@ public class task3 {
         }
 
         public void start() {
-            startTime = System.currentTimeMillis();
+            this.startTime = System.currentTimeMillis();
         }
 
         public void stop() {
-            endTime = System.currentTimeMillis();
+         this.endTime = System.currentTimeMillis();
         }
     }
 }
