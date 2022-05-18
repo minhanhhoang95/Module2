dfaa16498bb48395a4419cfdb882ad68b8d43e9b
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-05-18 14:23:39 +0700

    "update"

--- Java/src/task/InsertionSort.java
+++ Java/src/task/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 2, 9, 12, 12, 1};
+//        int[] arr = {5, 4, 2, 9, 12, 12, 1};
 //        for (int i = 0; i < arr.length; i++) {
 //            for (int j = i + 1; j < arr.length; j++) {
 //                if (arr[i] > arr[j]) {
@@ -15,15 +15,35 @@ public class InsertionSort {
 //            }
 //        }
 //        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp ;
-                j=j-1;
+
+
+
+//        int[] arr = {5, 4, 2, 9, 12, 12, 1};
+//        for (int i = 0; i < arr.length; i++) {
+//            int j = i;
+//            while (j > 0 && arr[j - 1] > arr[j]) {
+//                int temp = arr[j];
+//                arr[j] = arr[j - 1];
+//                arr[j - 1] = temp;
+//                j = j - 1;
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+
+
+
+        //for(;;)
+        int[] arr = {5, 4, 2, 9, 12, 12, 1};
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; arr[j - 1] > arr[j]; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
             }
         }
         System.out.println(Arrays.toString(arr));
+
     }
 }
