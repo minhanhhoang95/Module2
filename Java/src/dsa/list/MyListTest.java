6a6dbdac85e8a163e794c840894dfd6b5a1ba22d
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-06-02 21:45:57 +0700

    "update DSA_array_linked_list"

---
+++ Java/src/dsa/list/MyListTest.java
@@ -0,0 +1,18 @@
+package dsa.list;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer>listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+        System.out.println("elememt 4 : "+ listInteger.get(4));
+        System.out.println("elememt 1 : "+ listInteger.get(1));
+        System.out.println("elememt 2 : "+ listInteger.get(2));
+//        listInteger.get(6);
+//        System.out.println("elememt 6 : "+ listInteger.get(6));
+
+    }
+}
