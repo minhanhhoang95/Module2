d10064797edce0829d2cf3ba3b2cf7cf394d79a1
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-06-02 21:40:25 +0700

    "update test"

---
+++ Java/src/test/calculator.java
@@ -0,0 +1,11 @@
+package test;
+
+public class calculator {
+    public  int add(int first, int second){
+        return first + second;
+    }
+
+    public  int sub(int first, int second){
+        return first - second;
+    }
+}
