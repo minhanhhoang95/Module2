1fe48fc06385fcde6beba5b87e49ec1a96a064b5
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-06-02 21:41:52 +0700

    "update clean"

---
+++ Java/src/cleancode/Cylinder.java
@@ -0,0 +1,21 @@
+package cleancode;
+
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        return getVolume(height, baseArea, perimeter);
+    }
+
+    private static double getVolume(int height, double baseArea, double perimeter) {
+        return perimeter * height + 2 * baseArea;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+}
