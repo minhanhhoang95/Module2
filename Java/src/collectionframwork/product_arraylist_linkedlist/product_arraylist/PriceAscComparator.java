40927afae610e85795c6042481e33264e3881cfa
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-06-08 09:02:08 +0700

    "update collection framwork"

---
+++ Java/src/collectionframwork/product_arraylist_linkedlist/product_arraylist/PriceAscComparator.java
@@ -0,0 +1,16 @@
+package collectionframwork.product_arraylist_linkedlist.product_arraylist;
+
+import java.util.Comparator;
+
+public class PriceAscComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product productOne, Product productTwo) {
+        if (productOne.getPrice() > productTwo.getPrice()) {
+            return 1;
+        } else if (productOne.getPrice() == productTwo.getPrice()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ Java/src/collectionframwork/product_arraylist_linkedlist/product_linkedlist/PriceASC.java
@@ -0,0 +1,17 @@
+package collectionframwork.product_arraylist_linkedlist.product_linkedlist;
+
+import java.util.Comparator;
+
+public class PriceASC implements Comparator<Product> {
+    @Override
+    public int compare(Product productOne, Product productTwo) {
+//        return productOne.getPrice() - productTwo.getPrice();
+        if (productOne.getPrice() < productTwo.getPrice()) {
+            return -1;
+        } else if (productOne.getPrice() == productTwo.getPrice()) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
